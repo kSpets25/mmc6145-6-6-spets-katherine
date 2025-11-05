7ad40bae1b622de54f86fc182515953957bb3719
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-11-05 11:17:56 -0600

    Update book.js
    added try catch to complete the tests.

--- pages/api/book.js
+++ pages/api/book.js
@@ -6,20 +6,15 @@ import db from '../../db'
 // this handler runs for /api/book with any request method (GET, POST, etc)
 export default withIronSessionApiRoute(
   async function handler(req, res) {
-    // TODO: On a POST request, add a book using db.book.add with request body 
-    // TODO: On a DELETE request, remove a book using db.book.remove with request body 
-    // TODO: Respond with 404 for all other requests
-    // User info can be accessed with req.session
-    // No user info on the session means the user is not logged in
    
     const { method } = req;
     const user = req.session.user;
     console.log("Session user:", req.session.user);
-    
+
+    try {
     // Require login
       if (!user) {
         return res.status(401).json({ error: "Unrecognized: Please log in first." }); 
-        
       }
      
       if ( method === "POST") {
@@ -34,14 +29,8 @@ export default withIronSessionApiRoute(
           req.session.destroy();
           
           return res.status(401).json({ error: "Unauthorized: User not found or cannot add book." });
-           // (error) => {
-          //  console.error("Book remove error:", error);
-           // return res.status(400).json({ error: error.message || "oh no" });
-          //}
-        }
-        
+        } 
           return res.status(200).json({ message: "Book added successfully", book: addedBook });
-         
       }
       
       if (method === "DELETE") {
@@ -57,10 +46,14 @@ export default withIronSessionApiRoute(
           return res.status(401).json({ error: "Unauthorized: User not found or cannot add book." });
         }
         return res.status(200).json({ message: "Book removed successfully", bookId: removedBook }); 
-       
-        }
-      
-      return res.status(404).end();
+      }
+        return res.status(404).end();
+
+      } catch (error) {
+        console.error("Book remove error:", error);
+         return res.status(400).json({ error: error.message || "oh no" });
+      }
+    
   },
   sessionOptions
   );
